0d782844f2a852fc0a88aa9e42a973ad5f88b605
1820158cpe <90688563+1820158cpe@users.noreply.github.com>
2022-07-11 10:14:10 +0800

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+print("Declare a variable for Student Name, Address, Year and Course")
+print("Print all the student information")
+
+studentName_158 = 'Jerou Jeuell Espelita'
+studentAdd_158 = 'Purok4, Brgy. Kayumanggi'
+studentYC_158 = 'CPE2A'
+
+print(studentName_158)
+print(studentAdd_158)
+print(studentYC_158)
